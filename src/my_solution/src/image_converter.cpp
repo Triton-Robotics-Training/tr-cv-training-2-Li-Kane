a6b93602681b07607ddcd8b29addd6ea4502a70e
Kane Li <kal036@ucsd.edu>
2023-11-28 13:00:36 -0800

    Completed Robocam-ish
    The cam works and can score points, but the code can definitely be cleaned up and made faster. Added a subscriber to the current angle to add or sub to.

--- src/my_solution/src/image_converter.cpp
+++ src/my_solution/src/image_converter.cpp
@@ -1,93 +1,69 @@
-#include <stdlib.h>
-#include <functional>
-
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
-#include "std_msgs/msg/header.hpp"
-#include "std_msgs/msg/float32.hpp"
-#include <cv_bridge/cv_bridge.h> // cv_bridge converts between ROS 2 image messages and OpenCV image representations.
-#include <image_transport/image_transport.hpp> // publish and subscribe to compressed image streams in ROS2
-#include <opencv2/opencv.hpp> // include everything about OpenCV
+#include "image_converter.h"
 
 //much thanks: https://gist.github.com/nightduck/a07c185faad82aeaacbfa87298d035c0
-class ImageConverter : public rclcpp::Node {
-public:
-    ImageConverter() : Node("image_converter") {
-        rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
-        pub_ = this->create_publisher<std_msgs::msg::Float32>("/desired_angle", 10);
-        sub_ = image_transport::create_subscription(
-                this, "robotcam",std::bind(&ImageConverter::imageCallback,
-                                           this, std::placeholders::_1), "raw", custom_qos);
+ImageConverter::ImageConverter() : Node("image_converter"){
+    rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
+    pub_ = this->create_publisher<std_msgs::msg::Float32>("desired_angle", 10);
+    curr_angle_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+            "current_angle", 10, std::bind(&ImageConverter::angleCallback, this, _1));
+    sub_ = image_transport::create_subscription(
+            this, "robotcam",std::bind(&ImageConverter::imageCallback,
+                                       this, _1), "raw", custom_qos);
     }
 
-private:
-    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_;
-    image_transport::Subscriber sub_;
-    cv::Mat nonZeros;
-    cv::Mat avg;
+    //current angle callback which updates the current angle
+    void ImageConverter::angleCallback(const std_msgs::msg::Float32::SharedPtr msg){
+        curr_angle = *msg;
+        //log curr_angle value
+        RCLCPP_INFO(this->get_logger(), "curr_angle: %f", curr_angle.data);
+    }
 
-    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg) {
+    void ImageConverter::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg) {
         cv_bridge::CvImagePtr cv_ptr;
-        try
-        {
-            //use cv_bridge to convert ROS image to OpenCV image
-            cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
-        }
-        catch (cv_bridge::Exception& e)
-        {
-            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-            return;
-        }
+        //use cv_bridge to convert ROS image to OpenCV image
+        cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
         //mask the image for the color red
         cv::Mat dstImage = cv::Mat(640, 640, CV_8UC3, cv::Scalar(255,255,255));
         inRange(cv_ptr->image, cv::Scalar(0, 0, 100), cv::Scalar(50, 50, 255), dstImage);
+
         //find the non-zero pixels and average them
         findNonZero(dstImage, nonZeros);
         double sumX = 0.0;
-        double sumY = 0.0;
         // Iterate over the non-zero points
         for (size_t i = 0; i < nonZeros.total(); i++) {
             sumX += nonZeros.at<cv::Point>(i).x;
-            sumY += nonZeros.at<cv::Point>(i).y;
         }
         // Calculate the average (x, y) coordinates
-        cv::Point2f avgPoint;
         if (nonZeros.total() > 0) {
             avgPoint.x = static_cast<float>(sumX / nonZeros.total());
-            avgPoint.y = static_cast<float>(sumY / nonZeros.total());
+            avgPoint.y = 0.0f;
         } else {
             // Handle the case where there are no red pixels
-            avgPoint.x = 0.0f;
-            avgPoint.y = 0.0f;
+            avgPoint.x = -1.0f;
+            avgPoint.y = -1.0f;
         }
         //log avgPoint coordinates
-        RCLCPP_INFO(this->get_logger(), "x: %f y: %f", avgPoint.x, avgPoint.y);
-
-        //draw a circle at the average location
-        //cv::circle(dstImage, avgPoint, 10, CV_RGB(255, 0, 0));
-        //show the image
-        //cv::imshow("View", dstImage);
-        cv::waitKey(30);
+        RCLCPP_INFO(this->get_logger(), "x: %f", avgPoint.x);
         //find ratio of image width to average x coordinate
-        double ratio = (320.0 - avgPoint.x) / 320;
-        //convert ratio to angle
-        double angle = ratio * 3.14159;
-        //convert angle to float32 to publish
-        float angle32 = static_cast<float>(angle);
-        RCLCPP_INFO(this->get_logger(), "angle: %f", angle32);
+        double ratio = (320.0 - avgPoint.x) / 320.0;
+        //if absolute value of ratio is less than 0.185, keep current angle
+        if(avgPoint.x == -1.0f){
+            angle = 0.0f;
+        }
+        else if (abs(ratio) < 0.185) {
+            angle = curr_angle.data;
+        }
+        else {
+            angle = static_cast<float>((ratio/8) * 0.785) + curr_angle.data;
+        }
+        RCLCPP_INFO(this->get_logger(), "new_angle: %f", angle);
+        curr_angle.data = angle;
 
         // Create a message object for publishing with a fixed value
         auto output_msg = std_msgs::msg::Float32();
-        output_msg.data = angle32;  // Set the desired angle to 0.0
+        output_msg.data = angle;
         pub_->publish(output_msg);
-        /*         *
-         * pub_ = image_transport::create_publisher(this, "modified_cam", custom_qos);
-         * sensor_msgs::msg::Image::SharedPtr outputMsg =
-                cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", dstImage).toImageMsg();
-
-        pub_.publish(*outputMsg.get()); */
     }
-};
 
 int main(int argc, char** argv)
 {
---
+++ src/my_solution/src/image_converter.h
@@ -0,0 +1,39 @@
+//
+// Created by bvox0102 on 11/28/23.
+//
+
+#ifndef TR_CV_TRAINING_2_LI_KANE_IMAGE_CONVERTER_H
+#define TR_CV_TRAINING_2_LI_KANE_IMAGE_CONVERTER_H
+
+#include <stdlib.h>
+#include <functional>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/header.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include <cv_bridge/cv_bridge.h> // cv_bridge converts between ROS 2 image messages and OpenCV image representations.
+#include <image_transport/image_transport.hpp> // publish and subscribe to compressed image streams in ROS2
+#include <opencv2/opencv.hpp> // include everything about OpenCV
+
+using std::placeholders::_1;
+
+class ImageConverter : public rclcpp::Node {
+public:
+    ImageConverter();
+private:
+    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg);
+    void angleCallback(const std_msgs::msg::Float32::SharedPtr msg);
+
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr curr_angle_sub_;
+
+    std_msgs::msg::Float32 curr_angle;
+    float angle;
+    image_transport::Subscriber sub_;
+    cv::Mat nonZeros;
+    cv::Mat avg;
+    cv::Point2f avgPoint;
+};
+
+#endif //TR_CV_TRAINING_2_LI_KANE_IMAGE_CONVERTER_H
